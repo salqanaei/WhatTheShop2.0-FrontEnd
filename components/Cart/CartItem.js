89a70caaeebfae6623283616c42930cadf806d37
moeabdal <mohammed.h.abdal@gmail.com>
2019-09-24 17:18:44 +0300

    profile update

--- components/Cart/CartItem.js
+++ components/Cart/CartItem.js
@@ -14,28 +14,6 @@ import {
 } from "native-base";
 import cartStore from "../../stores/cartStore";
 
-
-const CartItem = ({ cart }) => {
-  return (
-    <ListItem style={{ borderBottomWidth: 0 }}>
-      <Left>
-        <Text style={{ color: "black", marginLeft: 16 }}> {cart.item} </Text>
-        <Text note style={{ marginLeft: 16 }}>
-          Quantity: {cart.quantity}
-        </Text>
-        <Text note style={{ marginLeft: 16 }}>
-          Price: {cart.price}
-        </Text>
-      </Left>
-      <Right>
-        <Button transparent onPress={() => cartStore.removeItemFromCart(item)}>
-          <Icon name="trash" style={{ color: "black", fontSize: 21 }} />
-        </Button>
-      </Right>
-    </ListItem>
-  );
-};
-
 class CartItem extends Component {
   state = {
     quantity: this.props.cart.quantity
@@ -87,6 +65,5 @@ class CartItem extends Component {
     );
   }
 }
-
 
 export default CartItem;
